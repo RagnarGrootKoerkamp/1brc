99719930e96aca07ec0147403ef9a4b7c80b4ba5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-01-04 00:12:08 +0100

    bytes instead of strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(slice_split_once)]
 use std::{collections::HashMap, env::args, io::Read};
 
 struct Record {
@@ -29,22 +30,31 @@ impl Record {
 
 fn main() {
     let filename = args().nth(1).unwrap_or("measurements.txt".to_string());
-    let mut data = String::new();
+    let mut data = vec![];
     {
         let mut file = std::fs::File::open(filename).unwrap();
-        file.read_to_string(&mut data).unwrap();
+        file.read_to_end(&mut data).unwrap();
+        assert!(data.pop() == Some(b'\n'));
     }
     let mut h = HashMap::new();
-    for line in data.trim().split('\n') {
-        let (name, value) = line.split_once(';').unwrap();
-        let value = value.parse::<f32>().unwrap();
+    for line in data.split(|&c| c == b'\n') {
+        let (name, value) = line.split_once(|&c| c == b';').unwrap();
+        let value = unsafe { std::str::from_utf8_unchecked(value) }
+            .parse::<f32>()
+            .unwrap();
         h.entry(name).or_insert(Record::default()).add(value);
     }
 
     let mut v = h.into_iter().collect::<Vec<_>>();
     v.sort_unstable_by_key(|p| p.0);
     for (name, r) in &v {
-        println!("{name}: {:.1}/{:.1}/{:.1}", r.min, r.avg(), r.max);
+        println!(
+            "{}: {:.1}/{:.1}/{:.1}",
+            std::str::from_utf8(name).unwrap(),
+            r.min,
+            r.avg(),
+            r.max
+        );
     }
     eprintln!("Num records: {}", v.len());
 }
